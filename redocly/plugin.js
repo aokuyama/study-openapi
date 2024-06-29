7793e003bb1406b2a88ecc0675d1d10f420a59df
aokuyama <ao980s@gmail.com>
2024-06-29 12:58:47 +0900

    update rules directory

--- redocly/plugin.js
+++ redocly/plugin.js
@@ -1,27 +1,8 @@
+fixedOperationId = require("./plugins/assertions/fixedOperationId");
+
 module.exports = {
   id: "local",
   assertions: {
-    fixedOperationId: (value, options, ctx) => {
-      const result = [];
-      for (const path of value) {
-        Object.entries(ctx.rawNode[path]).forEach(([method, operation]) => {
-          const fixedOperationId = getFixedOperationId(path, method);
-          if (operation.operationId !== fixedOperationId) {
-            result.push({
-              message: fixedOperationId,
-              location: ctx.baseLocation.child([path, method, "operationId"]),
-            });
-          }
-        });
-      }
-
-      return result;
-    },
+    fixedOperationId: fixedOperationId,
   },
 };
-const getFixedOperationId = (path, method) =>
-  [method]
-    .concat(path.split("/"))
-    .filter(Boolean)
-    .map((s) => s.replace("{", "[").replace("}", "]"))
-    .join("-");
